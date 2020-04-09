b162723e78914497f3ce5926c1cd1404c9a0bf9e
Pablo Chico de Guzman <pchico83@gmail.com>
2020-04-10 00:24:37 +0200

    Simplify sample

--- README.md
+++ README.md
@@ -1,34 +1,23 @@
-# Getting Started on Okteto with Stacks
+# Getting Started on Okteto Cloud with Stacks
 
-This example shows how to leverage [Okteto](https://github.com/okteto/okteto) to deploys the Voting App in Kubernetes using Okteto Stacks. 
+This example shows how to leverage [Okteto](https://github.com/okteto/okteto) to deploys the Voting App on Okteto Cloud using [Stacks](https://okteto.com/docs/cloud/stacks). 
 
-<!-- This is the application used for the [How to Develop and Debug PHP Applications in Kubernetes](https://okteto.com/blog/how-to-develop-php-apps-in-kubernetes/) blog post. -->
-
-The Voting App is a simple distributed application running across multiple containers. Containers are implemented using Python, Node.js and Java, with Redis for messaging and Postgres for storage.
-
-![Architecture diagram](architecture.png)
-
-* A front-end web app in [Python](/vote) which lets you vote between two options.
-* A [Redis](https://hub.docker.com/_/redis/) queue which collects new votes.
-* A [Java](/worker/src/main) worker which consumes votes and stores them in...
-* A [Postgres](https://hub.docker.com/_/postgres/) database backed by a volume.
-* A [Node.js](/result) webapp which shows the results of the voting in real time.
-
+The Voting App is a simple web application implemented using Python, with Redis for storage.
 
 ## Deploy the Voting App
 
-```
-okteto stack deploy
+```console
+$ okteto stack deploy
 ```
 
 ## Deploy the Voting App rebuilding containers
 
-```
-okteto stack deploy --build
+```console
+$ okteto stack deploy --build
 ```
 
 ## Destroy the Voting App
 
-```
-okteto stack destroy
+```console
+$ okteto stack destroy
 ```
--- vote/app.py
+++ vote/app.py
@@ -1,41 +1,71 @@
 from flask import Flask, render_template, request, make_response, g
-from redis import Redis
+from redis import Redis, RedisError
+
 import os
 import socket
 import random
 import json
+import collections
 
-option_a = os.getenv('OPTION_A', "Cats")
-option_b = os.getenv('OPTION_B', "Dogs")
 hostname = socket.gethostname()
 
+redis = Redis(host="redis", db=0)
 app = Flask(__name__)
 
-def get_redis():
-    if not hasattr(g, 'redis'):
-        g.redis = Redis(host="redis", db=0, socket_timeout=5)
-    return g.redis
+def getOptions():
+    option_a = "Cats"
+    option_b = "Dogs"
+    return option_a, option_b
 
 @app.route("/", methods=['POST','GET'])
 def hello():
-    voter_id = hex(random.getrandbits(64))[2:-1]
-    vote = None
+    option_a, option_b = getOptions()
+
+    try:
+        votesA = int(redis.get(option_a) or 0) 
+        votesB = int(redis.get(option_b) or 0)
+    except RedisError:
+        votesA = "<i>cannot connect to Redis, counter disabled</i>"
+        votesB = "<i>cannot connect to Redis, counter disabled</i>"
 
     if request.method == 'POST':
-        redis = get_redis()
-        vote = request.form['vote']
-        data = json.dumps({'voter_id': voter_id, 'vote': vote})
-        redis.rpush('votes', data)
+        try:
+            vote = request.form['vote']
+            if vote == "a":
+                votesA = redis.incr(option_a)
+            else:
+                votesB = redis.incr(option_b)
+        except Exception as e:
+            print(e)
+            votesA = "<i>An error occured</i>"
+            votesB = "<i>An error occured</i>"
+
+    with open('/var/run/secrets/kubernetes.io/serviceaccount/namespace', 'r') as fp:
+        namespace = fp.read()
 
     resp = make_response(render_template(
         'index.html',
         option_a=option_a,
         option_b=option_b,
         hostname=hostname,
-        vote=vote,
+        namespace=namespace,
+        votes_a=votesA,
+        votes_b=votesB,
     ))
     return resp
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80, debug=True, threaded=True)
+    extra_files = []
+    if "development" == os.getenv("FLASK_ENV"):
+        app.jinja_env.auto_reload = True
+        app.config['TEMPLATES_AUTO_RELOAD'] = True
+        extra_files=[
+            "./static/stylesheets/style.css"
+        ]
+
+    app.run(
+        host='0.0.0.0',
+        port=8080,
+        extra_files=extra_files
+    )
