2a0c1bb705682ab2f5d254ff8d25a504b3fcf539
mbell4 <mark@okteto.com>
2022-12-11 22:54:54 -0700

    Update app.py

--- vote/app.py
+++ vote/app.py
@@ -13,8 +13,8 @@ redis = Redis(host="redis", db=0)
 app = Flask(__name__)
 
 def getOptions():
-    option_a = "tomatoes"
-    option_b = "oranges"
+    option_a = "Limes"
+    option_b = "Lemons"
     return option_a, option_b
 
 @app.route("/", methods=['POST','GET'])
